a6c2c50a726cf87ad1526985940c276e57a1b2b8
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2019-08-26 20:44:10 -0500

    Updates

---
+++ precache-manifest.e08ca88c4c52cd24da1da9d9e87f2341.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37489a57af1172130fd18949c6045dd",
+    "url": "/markov-words/index.html"
+  },
+  {
+    "revision": "833a346d725d8c1b08ee",
+    "url": "/markov-words/static/css/2.d8ef342c.chunk.css"
+  },
+  {
+    "revision": "a408ca59e4c56532afa7",
+    "url": "/markov-words/static/css/main.a37ef056.chunk.css"
+  },
+  {
+    "revision": "833a346d725d8c1b08ee",
+    "url": "/markov-words/static/js/2.a9ef668d.chunk.js"
+  },
+  {
+    "revision": "a408ca59e4c56532afa7",
+    "url": "/markov-words/static/js/main.1af2b187.chunk.js"
+  },
+  {
+    "revision": "46f1b53c14fc30ecb56d",
+    "url": "/markov-words/static/js/runtime~main.c01cd086.js"
+  },
+  {
+    "revision": "14c590d124662eb50efa4c00f027b79c",
+    "url": "/markov-words/static/media/fa-brands-400.14c590d1.eot"
+  },
+  {
+    "revision": "3e1b2a654a784ceb385157140b4ccd71",
+    "url": "/markov-words/static/media/fa-brands-400.3e1b2a65.woff2"
+  },
+  {
+    "revision": "5e8aa9ea0ebcd2218178f554cdd8e6b0",
+    "url": "/markov-words/static/media/fa-brands-400.5e8aa9ea.ttf"
+  },
+  {
+    "revision": "bfa9c38bd6081dafe7278dedc4aad1d9",
+    "url": "/markov-words/static/media/fa-brands-400.bfa9c38b.svg"
+  },
+  {
+    "revision": "df02c782834b113d605d8329368737b4",
+    "url": "/markov-words/static/media/fa-brands-400.df02c782.woff"
+  },
+  {
+    "revision": "285a9d2a28886ad64c4e45cbd733cf7c",
+    "url": "/markov-words/static/media/fa-regular-400.285a9d2a.ttf"
+  },
+  {
+    "revision": "5623624dd1b017c66c29d1ac69cdcfa3",
+    "url": "/markov-words/static/media/fa-regular-400.5623624d.woff"
+  },
+  {
+    "revision": "95f13e0be408d117bd3d9b366084a3ef",
+    "url": "/markov-words/static/media/fa-regular-400.95f13e0b.svg"
+  },
+  {
+    "revision": "aa66d0e0e38c75666e98db33abae955e",
+    "url": "/markov-words/static/media/fa-regular-400.aa66d0e0.eot"
+  },
+  {
+    "revision": "ac21cac3f22cc9642f5af32e0c750797",
+    "url": "/markov-words/static/media/fa-regular-400.ac21cac3.woff2"
+  },
+  {
+    "revision": "3ded831d708bf90b2da86756551b5c1c",
+    "url": "/markov-words/static/media/fa-solid-900.3ded831d.woff"
+  },
+  {
+    "revision": "42e1fbd2cf655a0b44a2dfae9ca2f8c1",
+    "url": "/markov-words/static/media/fa-solid-900.42e1fbd2.eot"
+  },
+  {
+    "revision": "6ed5e3bc9018d2a03e2f1fadbf8a0a4a",
+    "url": "/markov-words/static/media/fa-solid-900.6ed5e3bc.svg"
+  },
+  {
+    "revision": "896e20e26ad07dc63c9526ed814117e9",
+    "url": "/markov-words/static/media/fa-solid-900.896e20e2.ttf"
+  },
+  {
+    "revision": "d6d8d5da9214dc7d46b297672a602d55",
+    "url": "/markov-words/static/media/fa-solid-900.d6d8d5da.woff2"
+  }
+]);

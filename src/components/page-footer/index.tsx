8e44f7f87338ee302ed85a43d1b71dc461e4c205
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2019-08-28 16:21:27 -0500

    Add more to the copyright footer

--- src/components/page-footer/index.tsx
+++ src/components/page-footer/index.tsx
@@ -3,9 +3,16 @@ import React from 'react'
 import styles from './pageFooter.module.sass'
 
 const Footer: React.FC = () => {
+  const years = (): string => {
+    const start = 2019
+    const current = new Date().getFullYear()
+    return current > start
+      ? `${start}–${current}`
+      : start.toString()
+  }
   return (
     <footer className={styles.footer}>
-      <p className={styles.copyright}>Built by Ian A. Cook, copyright 2019 under the AGPL-3.0 license. See the project on <a href='https://github.com/nai888/markov-words' target='_blank' rel='noopener noreferrer'>GitHub</a>.</p>
+      <p className={styles.copyright}>Built by <a href='https://github.com/nai888' target='_blank' rel='noopener noreferrer'>Ian A.&nbsp;Cook</a>, copyright © {years()} under the <a href='https://github.com/nai888/markov-words/blob/master/LICENSE' target='_blank' rel='noopener noreferrer'>AGPL-3.0 license</a>. See the project on <a href='https://github.com/nai888/markov-words' target='_blank' rel='noopener noreferrer'>GitHub</a>.</p>
     </footer>
   )
 }

8fa5ed253dc4cb7377438d17cb02e822eafd3a65
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2020-04-19 12:44:59 -0500

    Transfer ownership to Susurrus LLC

--- src/components/page-footer/index.tsx
+++ src/components/page-footer/index.tsx
@@ -12,7 +12,7 @@ const Footer: React.FC = () => {
   }
   return (
     <footer className={styles.footer}>
-      <p className={styles.copyright}>Built by <a href='https://github.com/nai888' target='_blank' rel='noopener noreferrer'>Ian A.&nbsp;Cook</a>, copyright © {years()} under the <a href='https://github.com/nai888/markov-words/blob/master/LICENSE' target='_blank' rel='noopener noreferrer'>AGPL-3.0 license</a>. See the project on <a href='https://github.com/nai888/markov-words' target='_blank' rel='noopener noreferrer'>GitHub</a>.</p>
+      <p className={styles.copyright}>Built by <a href='https://github.com/nai888' target='_blank' rel='noopener noreferrer'>Ian A.&nbsp;Cook</a>, copyright © {years()} under the <a href='https://github.com/Susurrus-LLC/markov-words/blob/master/LICENSE' target='_blank' rel='noopener noreferrer'>AGPL-3.0 license</a>. See the project on <a href='https://github.com/Susurrus-LLC/markov-words' target='_blank' rel='noopener noreferrer'>GitHub</a>.</p>
     </footer>
   )
 }

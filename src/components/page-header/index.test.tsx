45e15513bb0e58fef66750cc2efeeeaaa62e0e37
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2020-04-21 10:57:47 -0500

    Add basic render tests
    Ensure the app and each component can render without crashing

--- src/components/page-header/index.test.tsx
+++ src/components/page-header/index.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './App'
+import Header from './'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<Header />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
